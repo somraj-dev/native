751f0a59b91ba6283ec784ac1e689e3f812cd673
somraj-dev <iitainsomraj701@gmail.com>
2026-08-12 01:36:02 +0530

    refactor: update root launchers to point directly to bin/publish executable

--- AxioVital.bat
+++ AxioVital.bat
@@ -1,4 +1,4 @@
 @echo off
 set "DOTNET_ROOT=%LOCALAPPDATA%\Microsoft\dotnet"
 set "PATH=%LOCALAPPDATA%\Microsoft\dotnet;%PATH%"
-start "" "%~dp0AxioVital-App\AxioVital.Desktop.exe"
+start "" "%~dp0axiovital-frontend\AxioVital.Desktop\bin\publish\AxioVital.Desktop.exe"
--- run-desktop.ps1
+++ run-desktop.ps1
@@ -1,3 +1,3 @@
 $env:DOTNET_ROOT = "$env:LOCALAPPDATA\Microsoft\dotnet"
 $env:Path = "$env:LOCALAPPDATA\Microsoft\dotnet;$env:Path"
-Start-Process "$PSScriptRoot\AxioVital-App\AxioVital.Desktop.exe"
+Start-Process "$PSScriptRoot\axiovital-frontend\AxioVital.Desktop\bin\publish\AxioVital.Desktop.exe"
